d0289c367abbf9866932037f82c0c57a9288a17d
Thitima <sokillt@gmail.com>
2015-01-27 13:54:17 +0700

    test bootstrap by path from web

--- Web/news/views.py
+++ Web/news/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.http import HttpResponse
+from django.template import RequestContext, loader
+
+def index(request):
+    #return HttpResponse("Personalized university news and announcement delivery service")
+    #template = loader.get_template('news/index.html')
+    return render(request,'news/index.html')
